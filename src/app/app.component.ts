97b541b6e7d3feface8e21be58bc988ce95fd56c
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-03-08 16:57:56 -0300

    uupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { Item } from './interfaces/iItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-lista-de-compras';
 
-  constructor() { }
+export class AppComponent implements OnInit {
+  title = 'app-lista-de-compras';
+  listaDeCompras!: Array<Item>;
+  
+  constructor(private ListaService: ListaDeCompraService) { }
+
+  ngOnInit(): void {
+      this.listaDeCompras = this.ListaService.getListaDeCompra();
+  }
 }
