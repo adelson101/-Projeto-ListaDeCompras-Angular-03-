f89d052030cc0f2cbb83e61b1c745211f50da091
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-03-08 21:28:50 -0300

    up

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
@@ -7,7 +7,7 @@ import { Item } from 'src/app/interfaces/iItem';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges {
   @Input()
   item!: Item;
 
@@ -17,7 +17,10 @@ export class ItemComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.item);
+    console.log('OnInit');
+  }
 
+  ngOnChanges(): void {
+    console.log('OnChanges');
   }
 }
